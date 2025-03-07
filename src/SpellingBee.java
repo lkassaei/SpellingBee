3369caf694ddafc725475eca2e3f14a96a982879
lkassaei <lily.kassaei@menloschool.org>
2025-03-06 18:49:51 -0800

    Completed generate() and added generateHelper(), as well as started sort() and merge() by adding helpers to them.

--- src/SpellingBee.java
+++ src/SpellingBee.java
@@ -45,12 +45,61 @@ public class SpellingBee {
     //  that will find the substrings recursively.
     public void generate() {
         // YOUR CODE HERE — Call your recursive method!
+        ArrayList<String> substrings = new ArrayList<String>();
+        // First and last must start at zero to be able to capture all substrings
+        words = generateHelper(substrings, 0, 0, letters);
+        for (String s : words) {
+            System.out.println(s);
+        }
+    }
+
+    public ArrayList<String> generateHelper(ArrayList<String> arr, int first, int last, String word) {
+        // Base case: If we have nothing more to check, return the array of substrings
+        if (first == word.length()) {
+            return arr;
+        }
+        // Second base case: If the last index exceeds the length of the word then move onto the next starting index and reset the last index
+        if (last >= word.length()) {
+            return generateHelper(arr, first + 1, first + 1, word);
+        }
+        // Add current substring to arraylist making sure to include last index
+        arr.add(word.substring(first, last + 1));
+        // Keep moving on in the word by increasing the last index
+        return generateHelper(arr, first, last + 1, word);
     }
 
     // TODO: Apply mergesort to sort all words. Do this by calling ANOTHER method
     //  that will find the substrings recursively.
     public void sort() {
         // YOUR CODE HERE
+        int[] sorted = new int[words.size()];
+        sortHelper(sorted, 0, words.size() -1);
+    }
+
+    public int[] sortHelper(int[] arr, int low, int high) {
+        if (high == low) {
+            int[] newArr = new int[1];
+            newArr[0] = arr[low];
+            return newArr;
+        }
+        int med = (high + low)/2;
+        int[] arr1 = sortHelper(arr, low, med);
+        int[] arr2 = sortHelper(arr, med+1, high);
+        return merge(arr1, arr2);
+    }
+
+    private int[] merge(int[] arr1, int[] arr2) {
+        int[] merged = new int[arr1.length + arr2.length];
+        int i = 0, j =0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                merged[i + j] = arr1[i++];
+            }
+            else {
+                merged[i+j] = arr2[j++];
+            }
+        }
+        return merged;
     }
 
     // Removes duplicates from the sorted list.
@@ -69,6 +118,23 @@ public class SpellingBee {
     //  If it is not in the dictionary, remove it from words.
     public void checkWords() {
         // YOUR CODE HERE
+        for (String w: words) {
+            checkWordsHelper(w, 0, DICTIONARY_SIZE -1);
+        }
+    }
+
+    public boolean checkWordsHelper(String word, int first, int last) {
+        int mid = (first + last)/2 + first;
+        if (first == last) {
+            return false;
+        }
+        if (DICTIONARY[mid].equals(word)) {
+            return true;
+        }
+        if (1 ==1) {// If dictionary[mid] >= word
+            return checkWordsHelper(word, first, mid -1);
+        }
+        return checkWordsHelper(word, mid + 1, last);
     }
 
     // Prints all valid words to wordList.txt
